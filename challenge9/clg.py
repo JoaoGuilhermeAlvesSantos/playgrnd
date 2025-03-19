6b21efb920dc8cbd503f7a0dd729d45696d4c1d0
joaoguilhermealvessantos <joaoguilhermeas453@gmail.com>
2025-03-19 13:46:19 -0300

    a

---
+++ challenge9/clg.py
@@ -0,0 +1,16 @@
+# 9 [] 9 [] 9 [] 9 = 100
+numbers = [9, 9, 9, 9]
+opts = ['+', '-', '*', '/', "%", "^", "&", "|", ">>", "<<"]
+
+for i in range(len(opts)):
+    for j in range(len(opts)):
+        for k in range(len(opts)):
+            try:
+                res = eval(f'{numbers[0]}{opts[i]}{numbers[1]}{opts[j]}{numbers[2]}{opts[k]}{numbers[3]}')
+                if res == 100:
+                    print(f'{numbers[0]}{opts[i]}{numbers[1]}{opts[j]}{numbers[2]}{opts[k]}{numbers[3]}', "=", res)
+
+            except:
+                pass
+# Result: some MF said that the answer concatenate 2 9's (come on...) to make a 99, so sum with a 9/9 to get your 100
+# True result (come on...) is that there is no way to get 100 with 4 9's and the operators above
