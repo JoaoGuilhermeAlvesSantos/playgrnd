08d32e275c91b5b8bca087de2afa395d9b0c13ae
joaoguilhermealvessantos <joaoguilhermeas453@gmail.com>
2025-04-18 12:02:21 -0300

    a

--- worldmap/main.py
+++ worldmap/main.py
@@ -13,27 +13,27 @@ df = pd.DataFrame(list(events.items()), columns=["country", "value"])
 
 # Define categorias nomeadas para os tiers
 def get_tier_label(val):
-    if val < 51:
-        return "Lowest (0-50)"
-    elif val < 201:
-        return "Low (51–200)"
-    elif val < 601:
-        return "Moderate (201–600)"
-    elif val < 1501:
-        return "High (601–1500)"
+    if val <= 50:
+        return "Lowest [0–50]"
+    elif val <= 200:
+        return "Low [51–200]"
+    elif val <= 600:
+        return "Moderate [201–600]"
+    elif val <= 1500:
+        return "High [601–1500]"
     else:
-        return "Highest (1500+)"
+        return "Highest [1500+]"
 
 
 df["tier"] = df["value"].apply(get_tier_label)
 
 # Paleta de cores correspondente aos nomes
 tier_colors = {
-    "Lowest (0-50)": "#FAF0CA",
-    "Low (51–200)": "#F4D35E",
-    "Moderate (201–600)": "#EE964B",
-    "High (601–1500)": "#F95738",
-    "Highest (1500+)": "#00171F",
+    "Lowest [0–50]": "#FAF0CA",
+    "Low [51–200]": "#F4D35E",
+    "Moderate [201–600]": "#EE964B",
+    "High [601–1500]": "#F95738",
+    "Highest [1500+]": "#00171F",
 }
 
 # Mapa com legenda categórica
